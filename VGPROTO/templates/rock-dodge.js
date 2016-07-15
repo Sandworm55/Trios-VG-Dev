1296511d1bce090fd9346510c40f7ff123ea4049
Jordan-Trios <sandworm55@hotmail.com>
2016-07-15 08:39:37 -0400

    finished refactoring of code

--- VGPROTO/templates/rock-dodge.js
+++ VGPROTO/templates/rock-dodge.js
@@ -29,55 +29,12 @@ $(document).ready(function() {
 			tempAsteroid.x += tempAsteroid.vx;
 			tempAsteroid.y += tempAsteroid.vy;
 
-			if (tempAsteroid.x - tempAsteroid.radius < 0) 
-			{
-				tempAsteroid.x = tempAsteroid.radius;
-				tempAsteroid.vx *= -1;
-			} 
-			else if (tempAsteroid.x + tempAsteroid.radius > canvas.width()) 
-			{
-				tempAsteroid.x = canvas.width() - tempAsteroid.radius;
-				tempAsteroid.vx *= -1;
-			}
+			leftRightWallCollision(tempAsteroid);
+			topBottomWallCollision(tempAsteroid);
+			checkAsteroidToAsteroidColision(tempAsteroid,i);
 
-			if (tempAsteroid.y - tempAsteroid.radius < 0) 
-			{
-				tempAsteroid.y = tempAsteroid.radius;
-				tempAsteroid.vy *= -1;
-			} 
-			else if (tempAsteroid.y + tempAsteroid.radius > canvas.height()) 
-			{
-				tempAsteroid.y = canvas.height() - tempAsteroid.radius;
-				tempAsteroid.vy *= -1;
-			}
+			drawAsteroid(tempAsteroid);
 
-			for (j = i + 1; j < asteroids.length; j++) 
-			{
-
-				var tempAsteroid2 = asteroids[j];
-
-				var dx = tempAsteroid2.x - tempAsteroid.x;
-				var dy = tempAsteroid2.y - tempAsteroid.y;
-				var distance = Math.sqrt(dx*dx + dy*dy);
-
-				if (distance < tempAsteroid2.radius + tempAsteroid.radius)
-				{
-					tempAsteroid.vx *= -1;
-					tempAsteroid.vy *= -1;
-					tempAsteroid2.vx *= -1;
-					tempAsteroid2.vy *= -1;
-				}
-			}
-
-			ctx.beginPath();
-			ctx.arc(
-				tempAsteroid.x,
-				tempAsteroid.y,
-				tempAsteroid.radius,
-				0,
-				Math.PI * 2,
-				false);
-			ctx.fill();
 		}
 
 		setTimeout(animate, 33);
@@ -133,4 +90,66 @@ $(document).ready(function() {
 			asteroids.push(new Asteroid(x, y, radius, vx, vy));
 		}
 	}
+	
+	function drawAsteroid(tempAsteroidVar)
+	{
+		ctx.beginPath();
+		ctx.arc(
+			tempAsteroidVar.x,
+			tempAsteroidVar.y,
+			tempAsteroidVar.radius,
+			0,
+			Math.PI * 2,
+			false);
+		ctx.fill();
+	}
+	
+	function checkAsteroidToAsteroidColision(tempAsteroidVar,iNum)
+	{
+		for (j = iNum + 1; j < asteroids.length; j++) 
+		{
+			var tempAsteroid2 = asteroids[j];
+
+			var dx = tempAsteroid2.x - tempAsteroidVar.x;
+			var dy = tempAsteroid2.y - tempAsteroidVar.y;
+			var distance = Math.sqrt(dx*dx + dy*dy);
+
+			if (distance < tempAsteroid2.radius + tempAsteroidVar.radius)
+			{
+				tempAsteroidVar.vx *= -1;
+				tempAsteroidVar.vy *= -1;
+				tempAsteroid2.vx *= -1;
+				tempAsteroid2.vy *= -1;
+			}
+		}
+	}
+	
+	function leftRightWallCollision(tempAsteroid)
+	{
+		if (tempAsteroid.x - tempAsteroid.radius < 0) 
+		{
+			tempAsteroid.x = tempAsteroid.radius;
+			tempAsteroid.vx *= -1;
+		} 
+		else if (tempAsteroid.x + tempAsteroid.radius > canvas.width()) 
+		{
+			tempAsteroid.x = canvas.width() - tempAsteroid.radius;
+			tempAsteroid.vx *= -1;
+		}
+	}
+	
+	function topBottomWallCollision(tempAsteroid)
+	{
+		if (tempAsteroid.y - tempAsteroid.radius < 0) 
+		{
+			tempAsteroid.y = tempAsteroid.radius;
+			tempAsteroid.vy *= -1;
+		} 
+		else if (tempAsteroid.y + tempAsteroid.radius > canvas.height()) 
+		{
+			tempAsteroid.y = canvas.height() - tempAsteroid.radius;
+			tempAsteroid.vy *= -1;
+		}
+	}
+	
 });
