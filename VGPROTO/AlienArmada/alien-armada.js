2916795f6a85454128c60dfdbcb98774bd33aad1
Jordan-Trios <sandworm55@hotmail.com>
2016-07-25 11:04:00 -0400

    set up basic structure for loading

--- VGPROTO/AlienArmada/alien-armada.js
+++ VGPROTO/AlienArmada/alien-armada.js
@@ -1,6 +1,66 @@
 $(document).ready(function ()
 {
-
+	var canvas = document.querySelector("canvas");
+	var ctx = canvas.getContext("2d");
+	
+	var LOADING = 0;
+	var PLAYING = 1;
+	var GAMEEND = 2;
+	var gameState = LOADING;
+	
+	var assetsToLoad = [];
+	var assetsLoaded = 0;
+	
+	var image = new Image();
+	image.src = "AlienArmada/resources/alienArmada.png";
+	image.addEventListener("load", loadHandler);
+	assetsToLoad.push(image);
 	
 	
+	function loadHandler()
+	{
+		assetsLoaded += 1;
+		
+		if (assetsLoaded == assetsToLoad.length)
+		{
+			gameState = PLAYING;
+			image.removeEventListener("load", loadHandler);
+		}
+	}
+	
+	function update() 
+	{
+		window.requestAnimationFrame(update);
+		switch (gameState)
+		{
+			case LOADING:
+				console.log("Loading...");
+				break;
+			case PLAYING:
+				playGame();
+				break;
+			case GAMEEND:
+				endGame();
+				break;
+			default:
+				console.log("Welp, shit went pear shaped on us....");
+		}
+	}
+	
+	function playGame()
+	{
+		
+	}
+	
+	function endGame()
+	{
+		
+	}
+	
+	function render()
+	{
+		
+	}
+	
+	update();
 });
