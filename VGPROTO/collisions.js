c8ca055d3f06d02d84458c713fab5c1532edb944
Jordan-Trios <sandworm55@hotmail.com>
2016-07-28 09:18:06 -0400

    created function hitTestPoint, that checks for hits on a spriteObject from a x and y cord

--- VGPROTO/collisions.js
+++ VGPROTO/collisions.js
@@ -1,18 +1,29 @@
-function hitTestRectangle(r1,r2){
+function hitTestPoint(px, py, r)
+{
 	var hit = false;
-	
+
+	if ( px > r.left() && px < r.right() && py > r.top() && py < r.bottom() )
+		hit = true;
+
+	return hit;
+}
+
+function hitTestRectangle(r1, r2)
+{
+	var hit = false;
+
 	var dx = r1.center().x - r2.center().x;
 	var dy = r1.center().y - r2.center().y;
-	
+
 	var sumHalfWidths = r1.halfWidth() + r2.halfWidth();
-	if (Math.abs(dx) < sumHalfWidths)
+	if ( Math.abs(dx) < sumHalfWidths )
 	{
 		var sumHalfHeights = r1.halfHeight() + r2.halfHeight();
-		
-		if (Math.abs(dy) < sumHalfHeights)
+
+		if ( Math.abs(dy) < sumHalfHeights )
 			hit = true;
 	}
-	
+
 	return hit;
-	
+
 }
